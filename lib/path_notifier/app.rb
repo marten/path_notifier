055508718c86ad7bbe55d2af8608a6a8d25d351b
Marten Veldthuis <marten@veldthuis.com>
2012-04-06 21:51:46 +0200

    Spruce up the map view with POIs and places, make loading async

--- lib/path_notifier/app.rb
+++ lib/path_notifier/app.rb
@@ -4,8 +4,25 @@ module PathNotifier
       @last_location = Models::Coordinate.last
       @coordinates = Models::Coordinate.all
       @groups = @coordinates.group_by {|i| [i.timestamp.year, i.timestamp.month, i.timestamp.day] }
+      @places   = Models::Place.all
       erb :map
     end
+
+    get '/routes.json' do
+			@coordinates = Models::Coordinate.all
+			@groups = @coordinates.group_by {|i| [i.timestamp.year, i.timestamp.month, i.timestamp.day].join('-') }
+			return @groups.to_json
+		end
+
+		get '/pois.json' do
+			@pois   = Models::POI.all
+			return @pois.to_json
+		end
+
+		get '/places.json' do
+      @places   = Models::Place.all
+      return @places.to_json
+		end
 
     post '/coordinates' do
       coordinate = Models::Coordinate.create(params[:coordinate])
